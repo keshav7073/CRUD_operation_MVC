86cdfe93ab6c632f17f79f33a60d25b92f9bd337
keshav7073 <Bahetikeshav32@gmail.com>
2024-03-19 00:19:25 +0530

    forst commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using CRUD_OperationsInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUD_OperationsInMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index(int id)
+        {
+            MVC_DBEntities1 dbcontxt = new MVC_DBEntities1();
+            List<Employee> empList = dbcontxt.Employees.Where(emp =>emp.DepartmentId==id).ToList();
+            return View(empList);
+        }
+        public ActionResult Details(int id)
+        {
+            MVC_DBEntities1 dbcontxt = new MVC_DBEntities1();
+            Employee emp = dbcontxt.Employees.FirstOrDefault(x=>x.EmployeeId==id);
+            return View(emp);
+        }
+    }
+}
